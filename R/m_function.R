a4503f8f03b815687f91ab408f7e0b8f4b343fb5
Julio Cesar de Azevedo Vieira <oilujcesar@gmail.com>
2025-04-07 11:15:33 -0300

    Cleaning files and linking with the methods function

--- R/m_function.R
+++ R/m_function.R
@@ -16,10 +16,17 @@ m_function = function(formula, data, fit_model, ...){
     stop("'data' must be data.frame format!")
   }
   if(fit_model %>% is.null()){
-    stop("objeto 'fit_model' must be one of model options")
+    stop("objeto 'fit_model' must be one of method options")
   }
   if(!fit_model %in% c("DT", "RF", "LG", "SVM", "NN")){
-    stop("objeto 'fit_model' must be one of model options")
+    stop("objeto 'fit_model' must be one of method options")
   }
-
+  
+  slice_methods(formula, data, fit_model, ..)
+  
+# if(fit_model == "DT"){DT_method(formula, data, fit_model, ...)}
+# if(fit_model == "RF"){RF_method(formula, data, fit_model, ...)}
+# if(fit_model == "LG"){LG_method(formula, data, fit_model, ...)}
+# if(fit_model == "SVM"){SVM_method(formula, data, fit_model, ...)}
+# if(fit_model == "NN"){NN_method(formula, data, fit_model, ...)}
 }
